54195888ec24a1c36fe6bc7e1b88f4c248b4ba07
Hadi <hadi@local.host>
2017-02-25 02:16:52 +1100

    Add _Kid_

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224135755) do
+ActiveRecord::Schema.define(version: 20170224151627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20170224135755) do
 
   create_table "families", force: :cascade do |t|
     t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "kids", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
